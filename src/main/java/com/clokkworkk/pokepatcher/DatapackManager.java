4de880a61199fcbb0b95c2b9c05523d70ff455aa
CBSchiffer <CAS464@pitt.edu>
2025-05-16 04:50:51 -0400

    Started DatapackManager

--- src/main/java/com/clokkworkk/pokepatcher/DatapackManager.java
+++ src/main/java/com/clokkworkk/pokepatcher/DatapackManager.java
@@ -1,4 +1,50 @@
 package com.clokkworkk.pokepatcher;
 
+import io.netty.buffer.ByteBuf;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
 public class DatapackManager {
+
+    private static final Path DATAPACK_PATH = Paths.get("run","config", "pokepatcher", "datapacks");
+    private static final Path OUTPUT_PATH = Paths.get("run", "config", "pokepatcher", "generated");
+
+    public static void packageDatapacks() {
+        try {
+            Files.createDirectory(OUTPUT_PATH);
+            Files.list(DATAPACK_PATH)
+                    .filter(Files::isDirectory)
+                    .forEach(DatapackManager::packageDatapack);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void packageDatapack(Path datapackPath) {
+        String modId = datapackPath.getFileName().toString().toLowerCase().replaceAll("[^a-z0-9_]", "_");
+        Path outputJar = OUTPUT_PATH.resolve(modId + ".jar");
+        try (JarOutputStream jos = new JarOutputStream(Files.newOutputStream(outputJar))) {
+            jos.putNextEntry(new JarEntry("fabric.mod.json"));
+            jos.write(generateFabricModJson(modId).getBytes());
+            jos.closeEntry();
+
+            copyDirectoryToJar(datapackPath.resolve("data"), "data/", jos);
+            copyDirectoryToJar(datapackPath.resolve("assets"), "assets/", jos);
+
+            PokePatcher.LOGGER.info("Packaged datapack: " + datapackPath.getFileName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void copyDirectoryToJar(Path sourceDir, String jarPathPrefix, JarOutputStream jos) {
+    }
+
+    private static String generateFabricModJson(String modId) {
+    }
 }
